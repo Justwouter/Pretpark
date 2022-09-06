cea1514b31f62ac587e5cfd45a334a5fa488f9e2
Justwouter <wouter2312@gmail.com>
2022-09-06 20:55:04 +0200

    Organisation

---
+++ Pretpark.Kaart/Coordinaat.cs
@@ -0,0 +1,19 @@
+namespace Pretpark;
+
+public struct Coordinaat{
+    public readonly int X;
+    public readonly int Y;
+
+    public Coordinaat(int x, int y){
+        this.X = x;
+        this.Y = y;
+    }
+
+    public static Coordinaat operator+(Coordinaat c1, Coordinaat c2) {
+        return new Coordinaat((c1.X+c2.X), (c1.Y+c2.Y));
+    }
+
+    public static Coordinaat operator-(Coordinaat van, Coordinaat naar){
+        return new Coordinaat((van.X-naar.X), (van.Y-naar.Y));
+    }
+}
---
+++ Pretpark.Kaart/KaartItem.cs
@@ -0,0 +1,21 @@
+namespace Pretpark;
+
+public abstract class KaartItem : Tekenbaar{
+    private Coordinaat _locatie;
+    public Coordinaat Locatie {get { return _locatie;} set {
+        if(value.X >= kaart.Breedte && value.Y >= kaart.Hoogte){
+            _locatie = value;
+        }}}
+    private Kaart kaart;
+
+    public KaartItem(Kaart kaart, Coordinaat _locatie){
+        this.kaart = kaart;
+        this._locatie = _locatie;
+        kaart.VoegItemToe(this);
+    }
+    
+    public void TekenConsole(ConsoleTekener t){
+        t.SchrijfOp(this._locatie, Karakter.ToString());
+    }
+    public abstract char Karakter{get;}
+}
