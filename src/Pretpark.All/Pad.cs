10bf2527ce62a3fec22e0d6b4d18745be880bba4
Justwouter <wouter2312@gmail.com>
2022-09-15 01:37:54 +0200

    Added some comments & fixed the float extention

--- src/Pretpark.All/Pad.cs
+++ src/Pretpark.All/Pad.cs
@@ -9,14 +9,13 @@ public class Pad : Tekenbaar{
     private List<Coordinaat> CachedItems = new List<Coordinaat>{};
 
     public float Lengte(){
-
         //Here because sometimes this method gets called before values are Cached and it messes with AddDebug.
         //Also ensured they can never be equal to van/naar.
         if(CachedItems.Count() == 0){
             CachedItems = new List<Coordinaat>{new Coordinaat(this.van.X-1,this.naar.Y-2), new Coordinaat(this.van.X-1,this.naar.Y-2)};
         }
 
-        //I believe I saw somewhere that already cached items shouldn't be calculated.
+        //Cached items shouldn't be calculated. Don't know if it's faster tho.
         if(!CachedItems[0].Equals(van) || !CachedItems[1].Equals(naar)){
             var DiffrenceX = Math.Abs(van.X - naar.X); //Side A
             var DiffrenceY = Math.Abs(van.Y - naar.Y); //Side B
@@ -29,17 +28,22 @@ public class Pad : Tekenbaar{
         return (float)(lengteBerekend);
     }
 
+    //Draws the path object to the field of the assigned chart
     public void TekenConsole(ConsoleTekener t){
         Coordinaat verschil = naar - van;
         for (int i = 0; i < 100; i++){
             t.SchrijfOp(van + new Coordinaat((int)(verschil.X * ((float)i / 100)), (int)(verschil.Y * ((float)i / 100))), "#");
         }
-        //t.SchrijfOp(FindTheMiddle(), Lengte().metSuffixen());
+        
         t.SchrijfOp(FindTheMiddle(), (1000 * Lengte()).metSuffixen());
+
+        //Custom Debug cords
         t.AddDebug(new Coordinaat((FindTheMiddle().X),FindTheMiddle().Y), CachedItems);
         
     }
 
+
+    //Finds the point in between two Cords.
     private Coordinaat FindTheMiddle(){
         var DiffrenceX = (int)Math.Round(van.X + (naar.X - van.X) * 0.5); 
         var DiffrenceY = (int)Math.Round(van.Y + (naar.Y - van.Y) * 0.5);
@@ -50,6 +54,8 @@ public class Pad : Tekenbaar{
 
 }
 
+
+//Old method w/suffixes higher than k
 // public class Float {
 //     private float? DigitNum{get;}
 
@@ -75,17 +81,19 @@ public class Pad : Tekenbaar{
 //     }
 // }
 
+
+//Provides an extention method to the float class to convert anything above 1k into the shortend {number:letter} format.
 public static class FloatExtention{
 
     public static string metSuffixen(this float number){
         if(number >= 1000){
-            float f = number/1000;
-            String s = String.Format("{0:0.0}K",f);
-            return s;
+            float divider = number/1000;
+            String LetterFormat = String.Format("{0:0.0}K",divider);
+            return LetterFormat;
         }
         else{
-            String s = String.Format("{0:0.0}K",number);
-            return s;
+            String LetterFormat = String.Format("{0:0.0}",number);
+            return LetterFormat;
         }
 
     }
